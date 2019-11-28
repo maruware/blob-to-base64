774a5f25e4ea7e46069fe3466bd4b8eb0f148b9b
maruware <me@maruware.com>
2019-11-28 18:01:39 +0900

    separate dataurl and base64

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-export const fileToBase64 = (file: File) => {
+export const dataUrlToBase64 = (dataUrl: string) => {
+  return dataUrl.replace(/data:.+;base64,/, '')
+}
+
+export const fileToDataUrl = (file: File) => {
   return new Promise<string>(resolve => {
     const fr = new FileReader()
     fr.onload = e => {
-      resolve(e.target.result as string)
+      const dataUrl = e.target.result as string
+      resolve(dataUrl)
     }
     fr.readAsDataURL(file)
   })
+}
+
+export const fileToBase64 = async (file: File) => {
+  const dataUrl = await fileToDataUrl(file)
+  return dataUrlToBase64(dataUrl)
 }
