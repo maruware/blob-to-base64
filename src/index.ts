ff1a15a80d10251e8952eaec24d2eafd2a6f2b4c
maruware <me@maruware.com>
2019-11-28 17:08:55 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 export const fileToBase64 = (file: File) => {
-  return new Promise<string>((resolve, reject) => {
+  return new Promise<string>(resolve => {
     const fr = new FileReader()
     fr.onload = e => {
-      if (e) {
-        reject(e)
-      } else {
-        resolve(e.target.result as string)
-      }
+      resolve(e.target.result as string)
     }
     fr.readAsDataURL(file)
   })
