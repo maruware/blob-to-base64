2a4f00c26e16ba58daaba63c2a8d1a160bad2664
maruware <me@maruware.com>
2019-11-28 18:48:34 +0900

    file -> blob

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ export const dataUrlToBase64 = (dataUrl: string) => {
   return dataUrl.replace(/data:.+;base64,/, '')
 }
 
-export const fileToDataUrl = (file: File) => {
+export const blobToDataUrl = (blob: Blob) => {
   return new Promise<string>(resolve => {
     const fr = new FileReader()
     fr.onload = e => {
       const dataUrl = e.target.result as string
       resolve(dataUrl)
     }
-    fr.readAsDataURL(file)
+    fr.readAsDataURL(blob)
   })
 }
 
-export const fileToBase64 = async (file: File) => {
-  const dataUrl = await fileToDataUrl(file)
+export const blobToBase64 = async (blob: Blob) => {
+  const dataUrl = await blobToDataUrl(blob)
   return dataUrlToBase64(dataUrl)
 }
